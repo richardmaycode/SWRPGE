ff690eba203da1f463c40959ae0816dcd0cecb13
Richard Wise <orsusbass@gmail.com>
2019-11-26 22:08:03 -0600

    characteristic routes and index page

---
+++ app/controllers/characteristics_controller.rb
@@ -0,0 +1,20 @@
+class CharacteristicsController < ApplicationController
+  def index
+    @characteristics = Characteristic.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ spec/views/characteristics/create.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "characteristics/create.html.haml", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
