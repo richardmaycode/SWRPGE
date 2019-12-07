dff2e817e453efeeb8093ecf3f28fd8d3ea499b9
Richard Wise <orsusbass@gmail.com>
2019-12-07 15:38:56 -0600

    root to encyclopedia/main

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   end
 
   get '/encyclopedia', to: redirect('/encyclopedia/main')
-  root to: 'encyclopedia/main'
+  root to: '/encyclopedia/main', controller: 'encyclopedia/pages', action: 'main'
 end
